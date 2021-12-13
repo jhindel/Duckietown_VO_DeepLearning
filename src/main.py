74385cdf780214b0010a43b60e2ca88d86dbf9a9
julia <julia.hindel@fau.de>
2021-12-13 17:58:34 -0500

    changed onnx version to 11 for GPU

--- src/main.py
+++ src/main.py
@@ -94,7 +94,7 @@ def save_model_onnx(model, args):
                       x,  # model input (or a tuple for multiple inputs)
                       filename,  # where to save the model (can be a file or file-like object)
                       export_params=True,  # store the trained parameter weights inside the model file
-                      opset_version=12,  # the ONNX version to export the model to
+                      opset_version=11,  # the ONNX version to export the model to
                       do_constant_folding=True,  # whether to execute constant folding for optimization
                       input_names=['input'],  # the model's input names
                       output_names=['output'],  # the model's output names
