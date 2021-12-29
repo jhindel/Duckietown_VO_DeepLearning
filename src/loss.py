ce54f13c94905c875d3be97ad7bd35b5b9079753
julia <julia.hindel@fau.de>
2021-12-30 00:13:11 +0100

    shuffle on

--- src/loss.py
+++ src/loss.py
@@ -4,9 +4,12 @@ criterion = torch.nn.MSELoss()
 
 def DeepVO_loss(relative_pose_change, relative_pose_change_pred, K):
     # automatically calculates mean over batches
-    loss = (criterion(relative_pose_change_pred[:, :, :2], relative_pose_change[:, :, :2]) +
-                        K * criterion(relative_pose_change_pred[:, :, 2], relative_pose_change[:, :, 2]))
+    # print(criterion(relative_pose_change[:, :, :2], relative_pose_change_pred[:, :, :2]))
+    # print(criterion(relative_pose_change[:, :, 2], relative_pose_change_pred[:, :, 2]))
+    loss_vector = (criterion(relative_pose_change[:, :, :2], relative_pose_change_pred[:, :, :2]) +
+                        K * criterion(relative_pose_change[:, :, 2], relative_pose_change_pred[:, :, 2]))
     # print("angle", criterion(relative_pose_change_pred[:, :, 2], relative_pose_change[:, :, 2]))
+    loss = torch.mean(loss_vector)
     return loss
 
 
