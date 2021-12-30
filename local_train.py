b074578aa86283e77cbd747c508ea884b2135163
julia <julia.hindel@fau.de>
2021-12-30 22:49:03 +0100

    local train updated

--- local_train.py
+++ local_train.py
@@ -8,8 +8,8 @@ if __name__ == '__main__':
     wandb.login()
 
     args ={"data_dir":"/Users/julia/Documents/UNI/Master/Montréal/AV/project/duckietown_visual_odometry/data/",
-           "model":"ConvNet", "train_split": train_dummy, "val_split": val_dummy, "test_split": test_dummy,
-        "checkpoint_path":'./checkpoint', "checkpoint":None, "bsize":32, "lr":0.001,
+           "model":"ConvNet", "model_name":"ConvNet.pt", "train_split": train_dummy, "val_split": val_dummy,
+           "test_split": test_dummy, "checkpoint_path":'./checkpoint', "checkpoint":None, "bsize":32, "lr":0.001,
         "weight_decay":1e-4, "trajectory_length":5, "dropout_p":0.5,
         "resize":64, "K":100, "epochs":1, "patience":40}
 
